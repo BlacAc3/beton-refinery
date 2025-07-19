13bf4b8d5cbac300a38883a8d8c3e719ea1d90d7
blacac3 <aceezeala@gmail.com>
2025-07-19 18:47:54 +0100

    fixed alignment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
           <h1 className="text-5xl text-white font-bold">About us</h1>
         </div>
       </section>
-      <section>
+      <section className="flex flex-col justify-center items-center">
         <div className="py-24">
           <p className="max-w-3xl mx-auto text-center text-blue-600 leading-loose">
             Beton Refinery is among the leading Kazakhstan oil and energy
@@ -20,16 +20,14 @@ const About = () => {
             and many other energy related industrial activities.
           </p>
         </div>
-        <div className="flex justify-center pb-20 w-full">
-          <a href="/contact" className=" text-white  flex items-center ">
-            <span className="bg-blue-600  hover:bg-blue-700 px-8 py-4 text-xl font-semibold">
-              Contact us
-            </span>
-            <span className="flex justify-center items-center text-white text-center w-full h-full bg-blue-400 hover:bg-blue-500 transition duration-300 ease-in-out text-2xl font-bold leading-none">
-              &rarr;
-            </span>
-          </a>
-        </div>
+        <a href="/contact" className=" text-white max-w-3xl flex items-center ">
+          <div className="bg-blue-600  hover:bg-blue-700 px-8 py-4 text-xl font-semibold">
+            Contact us
+          </div>
+          <span className="flex justify-center items-center text-white text-center w-full h-full bg-blue-400 hover:bg-blue-500 transition duration-300 ease-in-out text-2xl font-bold leading-none">
+            &rarr;
+          </span>
+        </a>
       </section>
     </>
   );
